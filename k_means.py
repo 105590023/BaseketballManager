2b4be6675516e9b91a48687b9f6a16058f49bd11
JamesRui <z54543968@gmail.com>
2019-05-29 22:53:43 +0800

    5種特徵點訓練模型

--- k_means.py
+++ k_means.py
@@ -1,113 +1,44 @@
 #%%
-import numpy as np 
-import matplotlib.pyplot as plt  
-
+import numpy as np
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn import cluster
 from sklearn.cluster import KMeans
+from sklearn.externals import joblib
+from sklearn.decomposition import PCA
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import MinMaxScaler
-import seaborn as sns
 
 # 讀取測試資料
-train_url = "http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/train.csv"
-train = pd.read_csv(train_url)
-test_url = "http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/test.csv"
-test = pd.read_csv(test_url)
+playerData = pd.read_csv("./csv_data.csv")
 
 # 列出輸入資料
-print("***** Train_Set *****")
-print(train.describe())
-print("\n")
-print("***** Test_Set *****")
-print(test.describe())
+# print("------- playerData -------")
+# print(playerData.describe())
+# print(playerData.head())
+# print("\n")
 
 # 列出資料標頭
-print(train.columns.values)
+# print(playerData.columns.values)
 
-# 列出輸入資料缺失總數
-print("*****In the train set*****")
-print(train.isna().sum())
-print("\n")
-print("*****In the test set*****")
-print(test.isna().sum())
-
-# 以平均值修補缺失值
-train.fillna(train.mean(), inplace=True)
-test.fillna(test.mean(), inplace=True)
-
-print("*****In the train set*****")
-print(train.isna().sum())
-print("\n")
-print("*****In the test set*****")
-print(test.isna().sum())
-
-#%%
-train['Ticket'].head()
-train['Cabin'].head()
-
-#%%
-# 關於Pclass的生存計數
-train[['Pclass', 'Survived']].groupby(['Pclass'], as_index=False).mean().sort_values(by='Survived', ascending=False)
-
-#%%
-# 關於性別的生存計數
-train[["Sex", "Survived"]].groupby(['Sex'], as_index=False).mean().sort_values(by='Survived', ascending=False)
-
-#%%
-# 關於SibSp的生存計數
-train[["SibSp", "Survived"]].groupby(['SibSp'], as_index=False).mean().sort_values(by='Survived', ascending=False)
-
-#%%
-# 年齡與倖存者”的關係圖
-g = sns.FacetGrid(train, col='Survived')
-g.map(plt.hist, 'Age', bins=20)
-
-#%%
-# Pclass和Survived功能如何與圖表相互關聯
-grid = sns.FacetGrid(train, col='Survived', row='Pclass', size=2.2, aspect=1.6)
-grid.map(plt.hist, 'Age', alpha=.5, bins=20)
-grid.add_legend();
-
-#%%
-# 數據類型
-train.info()
-
-#%%
-# 刪除功能
-train = train.drop(['Name','Ticket', 'Cabin','Embarked'], axis=1)
-test = test.drop(['Name','Ticket', 'Cabin','Embarked'], axis=1)
-
-#%%
-# 將“性別”特徵轉換為數字特徵
-labelEncoder = LabelEncoder()
-labelEncoder.fit(train['Sex'])
-labelEncoder.fit(test['Sex'])
-train['Sex'] = labelEncoder.transform(train['Sex'])
-test['Sex'] = labelEncoder.transform(test['Sex'])
+# 查看資料類型
+# playerData.info()
 
 #%%
 # 訓練K-Means模型
-# drop()功能從數據中刪除Survival列
-X = np.array(train.drop(['Survived'], 1).astype(float))
-y = np.array(train['Survived'])
+data = playerData[['PTS', 'TRB', 'AST', 'STL', 'BLK']]
+pca = PCA(n_components=2)
+reduced_X = pca.fit_transform(data)
 
 # 構建K-Means模型
-kmeans = KMeans(n_clusters=2) # You want cluster the passenger records into 2: Survived or Not survived
+kmeans = KMeans(n_clusters=5)
 
 # 把0 - 1作為所有特徵的統一值範圍
 scaler = MinMaxScaler()
-X_scaled = scaler.fit_transform(X)
+X_scaled = scaler.fit_transform(reduced_X)
 
 kmeans.fit(X_scaled)
 
-#%%
-# 模型的準確性
-correct = 0
-for i in range(len(X)):
-    predict_me = np.array(X[i].astype(float))
-    predict_me = predict_me.reshape(-1, len(predict_me))
-    prediction = kmeans.predict(predict_me)
-    if prediction[0] == y[i]:
-        correct += 1
-
-print(correct/len(X))
+plt.scatter(X_scaled[:, 0], X_scaled[:, 1], c=kmeans.labels_)
+plt.show()
--- matplotlib_test.py
+++ matplotlib_test.py
@@ -1,11 +1,27 @@
-import numpy as np 
+from sklearn.cluster import KMeans
+from sklearn.externals import joblib
+from sklearn import cluster
+import numpy as np
 import matplotlib.pyplot as plt
+ 
+data = np.random.rand(100,2)
+estimator=KMeans(n_clusters=3)
 
-# Compute the x and y coordinates for points on a sine curve 
-x = np.arange(0, 3 * np.pi, 0.1) 
-y = np.sin(x) 
-plt.title("sine wave form") 
-
-# Plot the points using matplotlib 
-plt.plot(x, y) 
-plt.show() 
+estimator.fit_predict(data)
+# res=estimator.fit_predict(data)
+lable_pred=estimator.labels_
+# centroids=estimator.cluster_centers_
+# inertia=estimator.inertia_
+#print res
+# print(lable_pred)
+# print(centroids)
+# print(inertia)
+ 
+for i in range(len(data)):
+    if int(lable_pred[i])==0:
+        plt.scatter(data[i][0],data[i][1],color='red')
+    if int(lable_pred[i])==1:
+        plt.scatter(data[i][0],data[i][1],color='black')
+    if int(lable_pred[i])==2:
+        plt.scatter(data[i][0],data[i][1],color='blue')
+plt.show()
